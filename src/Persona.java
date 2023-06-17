ed22559f3d07a2657f55d60a0eb8023ba7b7ae81
MargaritaAlvarezGonz <margarita.alvarez3105@gmail.com>
2023-06-17 14:51:37 -0600

    Created new project

---
+++ src/Persona.java
@@ -0,0 +1,21 @@
+public class Persona {
+    private int id;
+    private String nombre;
+    private String tel;
+    private String email;
+    private static int numeroPersonas = 0;
+
+    //Constructor vacio
+
+    public Persona(){
+        this.id = ++Persona.numeroPersonas;
+    }
+    // Constructor con argumentos
+    public Persona(String nombre, String tel, String email){
+        this();
+        this.nombre = nombre;
+        this. tel = tel;
+        this.email = email;
+    }
+
+}

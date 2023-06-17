000aed6b738a9b6380bad25e496ff41f420a2468
MargaritaAlvarezGonz <margarita.alvarez3105@gmail.com>
2023-06-17 15:02:33 -0600

    Added get-set method

--- src/Persona.java
+++ src/Persona.java
@@ -10,12 +10,61 @@ public class Persona {
     public Persona(){
         this.id = ++Persona.numeroPersonas;
     }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getTel() {
+        return tel;
+    }
+
+    public void setTel(String tel) {
+        this.tel = tel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
     // Constructor con argumentos
     public Persona(String nombre, String tel, String email){
         this();
         this.nombre = nombre;
         this. tel = tel;
         this.email = email;
+
+    public int getId(){
+        return id;
+        }
+
+    public void setId(int id){
+        this.id = id;
+        }
+
+
+    }
+
+
+
+
+
     }
 
 }
